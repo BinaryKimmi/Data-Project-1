9f3f5f71aac4bba154541965acfa9c9c733525ef
Kimberley Whyte <kimberleywhyte@Kimberleys-MacBook-Air.local>
2026-02-12 13:22:49 -0500

    made a data dictionary very proud

--- video_stats.py
+++ video_stats.py
@@ -38,8 +38,6 @@ def get_playlist_id():
         raise e
 
 
-
-
 def get_video_ids(playlistId):
     
     video_ids = []
@@ -76,6 +74,54 @@ def get_video_ids(playlistId):
         raise e
 
 
+def extract_video_data(video_ids):
+
+    extracted_data = []
+
+    def batch_list(video_id_list, batch_size):
+    ###Will loop through the list in batches of size-batch_size 
+        for video_id in range(0, len(video_id_list), batch_size):
+            yield video_id_list[video_id: video_id + batch_size]
+
+    ##Loop through the entire list in batches, at each batch concatenate
+    try:
+        for batch in batch_list(video_ids, maxResults):
+            video_ids_str = ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}" 
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            ###print in json format
+            data = response.json()
+
+            for item in data.get('items',[]):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+            ##Define data dictionary that will take data from the above loop
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet["title"],
+                    "publishedAt": snippet["publishedAt"],
+                    "duration": contentDetails["duration"],
+                    ##The None is if there are videos with no stated attributes, then just put "none"
+                    "viewCount": statistics.get("viewCount", None),
+                    "likeCount": statistics.get("likeCount", None),
+                    "commentCount": statistics.get("commentCount", None),
+                }
+
+                extracted_data.append(video_data)
+
+        return extracted_data
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
 
 
 
@@ -87,9 +133,9 @@ def get_video_ids(playlistId):
 ###This allows you to control which parts of the code execute in different contexts. 
 
 if __name__ == "__main__":
-    #print("get_playlist_id will be executed")
     playlistId = get_playlist_id()
+    video_ids = get_video_ids(playlistId)
+    print(extract_video_data(video_ids))
     #print(get_video_ids(playlistId))
 
-#else:
-#    print("get_playlist_id wont be executed")
+
